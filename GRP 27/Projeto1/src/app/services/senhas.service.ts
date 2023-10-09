0b64233546200c0afb35777d53f2191cf9ae05f3
pmanso <pedromanso9@gmail.com>
2023-10-08 22:06:35 -0300

    alterações

--- GRP 27/Projeto1/src/app/services/senhas.service.ts
+++ GRP 27/Projeto1/src/app/services/senhas.service.ts
@@ -5,6 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class SenhasService {
 
+  //contadores
+
   public senhasGeral: number = 0;
   public senhasPrior: number = 0;
   public senhasExame: number = 0;
@@ -13,14 +15,37 @@ export class SenhasService {
   public atendidosPrior: number = 0;
   public atendidosExame: number = 0;
   public atendidosTotal:number = 0;
+
+
   public jaAtendi:number = 0;
-  public atendCounter:number = 0;
-  public senhasArray: { [key: string]: string[] } = {
+  public senhasArray: { [key: string]: {senhaI: string, hora: string, data: string}[] } = {
     'SG': [],
     'SP': [],
     'SE': [],
   };
   public inputNovaSenha: string = '';
+    
+
+ //Começo data
+
+  public inputData: string =
+    new Date().getDate().toString().padStart(2, '0') +
+    '/' + 
+    new Date().getMonth().toString().padStart(2, '0') +
+    '/' +
+    new Date().getFullYear().toString().substring(2, 4) ;
+
+  public inputHora: string = 
+    new Date().getHours().toString().padStart(2, '0') +
+    ':' +
+    new Date().getUTCMinutes().toString().padStart(2, '0') ;
+
+//Final data
+
+
+  public inputGuiche: number = Math.random() * 15;
+
+
 
   somaGeral() { this.senhasGeral++; this.senhasTotal++; }
   somaPrior() { this.senhasPrior++; this.senhasTotal++; }
@@ -34,22 +59,22 @@ export class SenhasService {
       this.inputNovaSenha =
         new Date().getFullYear().toString().substring(2, 4) +
         new Date().getMonth().toString().padStart(2, '0') +
-        new Date().getDay().toString().padStart(2, '0') +
+        new Date().getDate().toString().padStart(2, '0') +
         '-' +
         tipoSenha +
         (this.senhasArray['SG'].length + 1).toString().padStart(2, '0');
-      this.senhasArray['SG'].push(this.inputNovaSenha);
+      this.senhasArray['SG'].push({senhaI: this.inputNovaSenha, hora:this.inputHora, data:this.inputData});
     
     }else if(tipoSenha == 'SP') {
       this.somaPrior();
       this.inputNovaSenha =
         new Date().getFullYear().toString().substring(2, 4) +
         new Date().getMonth().toString().padStart(2, '0') +
-        new Date().getDay().toString().padStart(2, '0') +
+        new Date().getDate().toString().padStart(2, '0') +
         '-' +
         tipoSenha +
         (this.senhasArray['SP'].length + 1).toString().padStart(2, '0');
-      this.senhasArray['SP'].push(this.inputNovaSenha);
+      this.senhasArray['SP'].push({senhaI: this.inputNovaSenha, hora:this.inputHora, data:this.inputData});
 
     
     }else if(tipoSenha == 'SE') {
@@ -57,24 +82,23 @@ export class SenhasService {
       this.inputNovaSenha =
         new Date().getFullYear().toString().substring(2, 4) +
         new Date().getMonth().toString().padStart(2, '0') +
-        new Date().getDay().toString().padStart(2, '0') +
+        new Date().getDate().toString().padStart(2, '0') +
         '-' +
         tipoSenha +
         (this.senhasArray['SE'].length + 1).toString().padStart(2, '0');
-      this.senhasArray['SE'].push(this.inputNovaSenha);
+      this.senhasArray['SE'].push({senhaI: this.inputNovaSenha, hora:this.inputHora, data:this.inputData});
     
     
     }
+    
     console.log(this.senhasArray)
   }
 
 
   atender(){
-    if(this.atendCounter % 2 == 0){
+    if(this.atendidosTotal % 2 == 0 && this.atendidosPrior < this.senhasPrior){
       this.atendidosTotal++
       this.atendidosPrior++
-      this.atendCounter++
-      console.log("prior")
     }else{
       this.checksGE()
     }
@@ -82,20 +106,20 @@ export class SenhasService {
   
 
   checksGE(){
-    if(this.jaAtendi == 0) {
+    if(this.jaAtendi == 0 && this.atendidosExame < this.senhasExame) {
       this.atendidosTotal++
       this.atendidosExame++
-      this.atendCounter++
       this.jaAtendi = 1
-      console.log("exame")
-    }else{
+    }else if(this.atendidosGeral < this.senhasGeral){
       this.atendidosTotal++
       this.atendidosGeral++
-      this.atendCounter++
       this.jaAtendi = 0
-      console.log("geral")
+    }else if(this.atendidosPrior < this.senhasPrior){ //caso exista mais de um prioritário
+      this.atendidosTotal++
+      this.atendidosPrior++
+    }else{
+      alert("Não é possível atender pacientes que não existem")
     }
   }
 }
-  
 
