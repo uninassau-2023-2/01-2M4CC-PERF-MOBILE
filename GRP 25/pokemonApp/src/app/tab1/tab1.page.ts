cbc2058e87b2f3cad8eec26f4a9931ec51dc2911
Rhyan314 <rhyanlima314@gmail.com>
2023-11-05 14:54:28 -0300

    Mobile

---
+++ GRP 25/pokemonApp/src/app/tab1/tab1.page.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { PokeAPIService } from '../services/poke-api.service';
+import { ViaCEPService } from '../services/via-cep.service';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+areaBuscarPokemon: string = '52011210';
+areaBusca: any = {
+  bairro: '',
+  localidade: '',
+  logradouro: '',
+  uf: ''
+};
+sla: string[] = []
+pokemon: any = {
+  nome: '',
+  img: '',
+  habilidade: '',
+  altura: '',
+  peso: ''
+}
+
+  constructor(
+    private pokeAPIService: PokeAPIService,
+    private ViaCEPService: ViaCEPService
+  ) {}
+
+  buscarPokemon(){
+    this.ViaCEPService.getViaCEPService(this.areaBuscarPokemon).subscribe((value) => {
+      this.areaBusca.logradouro = JSON.parse(JSON.stringify(value)) ['logradouro'];
+      this.areaBusca.bairro = ', ' + JSON.parse(JSON.stringify(value)) ['bairro'];
+      this.areaBusca.localidade = ' - ' + JSON.parse(JSON.stringify(value)) ['localidade'];
+      this.areaBusca.uf = '-' + JSON.parse(JSON.stringify(value)) ['uf'];
+    });
+
+    this.pokeAPIService.getPokeAPIService().subscribe((value) => {
+      this.pokemon.nome = JSON.parse(JSON.stringify(value)) ['name'];
+      this.pokemon.img = JSON.parse(JSON.stringify(value)) ['id'];
+      this.pokemon.habilidade = (JSON.parse(JSON.stringify(value)) ['abilities']).length -1;
+      this.pokemon.altura = JSON.parse(JSON.stringify(value)) ['height'];
+      this.pokemon.peso = JSON.parse(JSON.stringify(value)) ['weight'];
+    });
+  }
+
+}
