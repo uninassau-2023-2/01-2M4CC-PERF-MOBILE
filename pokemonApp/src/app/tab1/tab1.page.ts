0a22eaa0dfd003506346b661f718c39d687433dd
elisams20@gmail.com <elisams20@gmail.com>
2023-11-05 17:31:34 -0300

    Atualizando o pokemonApp

--- pokemonApp/src/app/tab1/tab1.page.ts
+++ pokemonApp/src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PokeAPIService } from '../services/poke-api.service';
+import { ViaCEPService } from '../services/via-cep.service';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  areaBuscarPokemon: string = '52011210';
+  areaBusca: any = {
+    bairro: '',
+    localidade: '',
+    logradouro: '',
+    uf: ''
+  };
+  randomPokemonId: number = 1;
 
-  constructor() {}
+  pokemonName: string = '';
+  pokemonImageUrl: string = '';
+  pokemonAbilitiesCount: number = 0;
+  pokemonHeight: number = 0;
+  pokemonWeight: number = 0;
+  pokemon: any = { abilities: [] };
 
+  constructor(
+    private pokeAPIService: PokeAPIService,
+    private viaCEPService: ViaCEPService
+  ) { }
+
+  buscarPokemon() {
+    this.viaCEPService.getViaCEPService(this.areaBuscarPokemon)
+      .subscribe((value) => {
+        this.areaBusca.logradouro = JSON.parse(JSON.stringify(value))['logradouro'];
+        this.areaBusca.bairro = ', ' + JSON.parse(JSON.stringify(value))["bairro"];
+        this.areaBusca.localidade = ' - ' + JSON.parse(JSON.stringify(value))['localidade'];
+        this.areaBusca.uf = '-' + JSON.parse(JSON.stringify(value))['uf'];
+      });
+
+    this.randomPokemonId = Math.floor(Math.random() * 100) + 1;
+    this.pokeAPIService.getPokeAPIService(this.randomPokemonId)
+      .subscribe((pokemon) => {
+        this.pokemon = pokemon;
+        this.atualizarDadosPokemon();
+      });
+  }
+
+  atualizarDadosPokemon() {
+    this.pokemonName = this.pokemon.name.toUpperCase();
+    this.pokemonImageUrl = this.pokemon.sprites.other.dream_world.front_default;
+    this.pokemonAbilitiesCount = this.pokemon.abilities.length;
+    this.pokemonHeight = this.pokemon.height;
+    this.pokemonWeight = this.pokemon.weight;
+  }
 }
