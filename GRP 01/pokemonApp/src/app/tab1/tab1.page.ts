32f614df8717abdc3ee895f98dca1585c25ec299
Diigs <117246328+Diigs@users.noreply.github.com>
2023-11-05 14:51:47 -0300

    att pokemonzinho
    pasta src

--- GRP 01/pokemonApp/src/app/tab1/tab1.page.ts
+++ GRP 01/pokemonApp/src/app/tab1/tab1.page.ts
@@ -1,12 +1,60 @@
 import { Component } from '@angular/core';
+import { PokeAPIService } from '../services/poke-api.service';
+import { ViaCEPService } from '../services/via-cep.service';
+
+interface procurarPokemon {
+  name: string;
+  abilities: any[];
+  sprites: {
+    front_default: string;
+  };
+  height: number;
+  weight: number;
+}
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page {
+  areaBuscarPokemon: string = '52011210';
+  areaBusca: any ={
+    bairro: '',
+    localidade: '',
+    logradouro: '',
+    uf: ''
+  };
+  acharPokemon: procurarPokemon = {
+    name: '',
+    abilities: [],
+    sprites: {
+      front_default: ''
+    },
+    height: 0,
+    weight: 0,
+  }
 
-  constructor() {}
-
+  constructor(
+    private pokeAPIService: PokeAPIService,
+    private viaCEPService: ViaCEPService
+  ) {}
+  buscarPokemon() {
+    this.viaCEPService.getViaCEPService(this.areaBuscarPokemon)
+      .subscribe((value) => {
+        this.areaBusca.logradouro = JSON.parse(JSON.stringify(value))['logradouro'];
+        this.areaBusca.bairro = ', ' + JSON.parse(JSON.stringify(value))['bairro'];
+        this.areaBusca.localidade = ' - ' + JSON.parse(JSON.stringify(value))['localidade'];
+        this.areaBusca.uf = '-' + JSON.parse(JSON.stringify(value))['uf'];
+      });
+      this.pokeAPIService.getPokeAPIService()
+        .subscribe((value: any) => {
+          this.acharPokemon.name = value.name;
+          this.acharPokemon.sprites = value.sprites;
+          this.acharPokemon.abilities = value.abilities.map((ability: any) => ability.ability.name);
+          this.acharPokemon.height = value.height;
+          this.acharPokemon.weight = value.weight;
+        });
+  } 
 }
